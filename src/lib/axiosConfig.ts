594336112e29e3381a9c81b320e3c235db649281
Shahoriar <shuvo1556@gmail.com>
2023-08-25 21:11:18 +0600

    sfg

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -6,10 +6,9 @@ import { apiUrl } from './const';
 const Axios = axios.create({
   baseURL: apiUrl,
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Methods': undefined,
-    "Access-Control-Allow-Headers": undefined,
+    "Access-Control-Allow-Origin" : "*",
+    "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
   }
 });
 
