bd05ea2aedf375120f3f0f8fdf7f6aef14fcc353
Shahoriar <shuvo1556@gmail.com>
2023-08-25 13:35:34 +0600

    dfg

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
-const apiUrl = 'https://basecamp-backend.vercel.app'
+const apiUrl = process.env.NODE_ENV === "production"? 'http://localhost:5000' :"https://basecamp-backend.vercel.app/"
 
 export { apiUrl };
