ec45de80a88b5db95dca2226b161fb28e87563f4
Shahoriar <shuvo1556@gmail.com>
2023-08-25 14:37:03 +0600

    sfg

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -7,6 +7,9 @@ const Axios = axios.create({
   baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Methods': undefined,
+    "Access-Control-Allow-Headers": undefined,
   }
 });
 
