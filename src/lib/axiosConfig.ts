c2694a6abcf7edf46246c558fb63758533d5d295
Shahoriar <shuvo1556@gmail.com>
2023-08-25 21:15:35 +0600

    sfg

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -5,11 +5,6 @@ import { apiUrl } from './const';
 
 const Axios = axios.create({
   baseURL: apiUrl,
-  headers: {
-    "Access-Control-Allow-Origin" : "*",
-    "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-  }
 });
 
 export default Axios;
