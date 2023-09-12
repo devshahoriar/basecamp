95d2ff823034f3c840bf2403028926cf7cb670a0
Shahoriar <shuvo1556@gmail.com>
2023-09-12 12:25:10 +0600

    sdf

--- src/components/shared/AvaterGroupt.tsx
+++ src/components/shared/AvaterGroupt.tsx
@@ -3,10 +3,12 @@ import Avater from './Avater'
 
 const AvaterGroupt = () => {
   const queryClient = useQueryClient()
+  const project:any = queryClient.getQueryData('project')
+  const members:any = queryClient.getQueryData('members')
   return (
     <div className="avatar-group -space-x-3">
-        {queryClient.getQueryData('project')?.admins?.map((a,i)=> <Avater key={i} className="!h-8 !w-8" src={a?.avatar} title={a?.name} /> )}
-        {queryClient.getQueryData('members')?.map((a,i)=> <Avater key={i} className="!h-8 !w-8" src={a?.avatar} title={a?.name} /> )}
+        {project?.admins?.map((a,i)=> <Avater key={i} className="!h-8 !w-8" src={a?.avatar} title={a?.name} /> )}
+        {members?.map((a,i)=> <Avater key={i} className="!h-8 !w-8" src={a?.avatar} title={a?.name} /> )}
       {/* <Avater className="!h-8 !w-8" src="https://picsum.photos/200" />
       <Avater className="!h-8 !w-8" src="https://picsum.photos/200" />
       <Avater className="!h-8 !w-8" src="https://picsum.photos/200" />
