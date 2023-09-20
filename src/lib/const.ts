9c7467486abf281a251ba451dd44833f0b79883a
Shahoriar <shuvo1556@gmail.com>
2023-09-20 08:13:40 +0600

    Update const.ts

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
-const apiUrl = 'http://localhost:5000' //"https://basecamp-h9qe.onrender.com"  //'http://localhost:5000'
+const apiUrl = 'https://basecamp-h9qe.onrender.com' //"https://basecamp-h9qe.onrender.com"  //'http://localhost:5000'
 
 export { apiUrl };
